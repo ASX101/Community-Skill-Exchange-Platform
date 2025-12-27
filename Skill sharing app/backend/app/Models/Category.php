bfee9c035a8677c8a88e693e8e10e22c049ead41
Asrar Azim <169088563+ASX101@users.noreply.github.com>
2025-12-28 00:25:42 +0600

    backend

---
+++ Skill sharing app/backend/app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'icon',
+    ];
+
+    public $timestamps = true;
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
+}
